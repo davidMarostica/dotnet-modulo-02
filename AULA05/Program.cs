d5b4b20dccfe2c0ec98c2b767d0dc9c2020b0249
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-04-14 10:46:47 -0300

    Usando valores especiais

---
+++ AULA05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+bool? desejaReceberEmnail = true;
+if (desejaReceberEmnail.HasValue)
+{
+    if (desejaReceberEmnail.Value && desejaReceberEmnail.Value == true)
+    {
+        Console.WriteLine("O usuário deseja receber email.");
+    }
+    else
+    {
+        Console.WriteLine("O usuário não deseja receber email.");
+    }
+}
+else
+{
+    Console.WriteLine("O usuário não respondeu sobre receber email.");
+}
--- AULA05/obj/Debug/net8.0/AULA05.AssemblyInfo.cs
+++ AULA05/obj/Debug/net8.0/AULA05.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AULA04")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AULA05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b35a03bfcbc70a971ca7626a2443348d84c1fc")]
-[assembly: System.Reflection.AssemblyProductAttribute("AULA04")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AULA04")]
+[assembly: System.Reflection.AssemblyProductAttribute("AULA05")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AULA05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
